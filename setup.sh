ba8c1513d848c6f4d3e62ddd5ddc7d4ed601575f
IllyasvielEin <785291773@qq.com>
2022-09-25 12:50:56 +0800

    change enviroment to rcssserver for developer

--- setup.sh
+++ setup.sh
@@ -34,14 +34,14 @@ elif [ ${release_name_short} == 20 ];then
     echo 'deb http://download.opensuse.org/repositories/science:/SimSpark/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/science:SimSpark.list
     curl -fsSL https://download.opensuse.org/repositories/science:SimSpark/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/science_SimSpark.gpg > /dev/null
     sudo apt update
-    sudo apt install rcssserver3d -y || (colorlog "Installation fail" && exit 1)
+    sudo apt install rcssserver3d-dev -y || (colorlog "Installation fail" && exit 1)
     colorlog "Installation success,\nIf a new version of simspark & rcssserver3d has been released, you can upgrade them by:\n\nsudo apt-get update && sudo apt-get install rcssserver3d" 32
 elif [ ${release_name_short} == 18 ];then
     echo "Add openSUSE source"
     echo 'deb http://download.opensuse.org/repositories/science:/SimSpark/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/science:SimSpark.list
     curl -fsSL https://download.opensuse.org/repositories/science:SimSpark/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/science_SimSpark.gpg > /dev/null
     sudo apt update
-    sudo apt install rcssserver3d -y || (colorlog "Installation fail" && exit 1)
+    sudo apt install rcssserver3d-dev -y || (colorlog "Installation fail" && exit 1)
     colorlog "Installation success,\nIf a new version of simspark & rcssserver3d has been released, you can upgrade them by:\n\nsudo apt-get update && sudo apt-get install rcssserver3d" 32
 else
     colorlog "Unknow or unsupporting release, please make sure your Ubuntu is in {18.04 ~ 22}" 31
